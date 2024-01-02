0d6a9a57fdf74a132b866c01985bc68d199299a7
Yvan Tcheuffa <tcheuffayvan@yahoo.com>
2024-01-02 04:11:15 +0100

    Remove versions on services and proxies

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
 	id("com.livadoo.services-conventions")
 }
 
-version = "0.0.1"
-
 dependencies {
 	implementation(project(":notification-service-proxy"))
 	implementation(projects.utils.springUtils)
